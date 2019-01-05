10e389e81338b9f02641b53d13f381fcc6ce9bb1
merveaman <steamed.zeppelin@gmail.com>
2019-01-05 15:57:28 +0200

    Update mm1.cpp

--- mm1.cpp
+++ mm1.cpp
@@ -20,13 +20,13 @@ double end_time; //Total time to simulate
 double Ta;      //Mean time between arrivals
 double Ts;     // Mean service time
 double time; // Simulation time
-double t1;  // Time for next event #1 (arrival)
-double t2; //Time for next event #2 (departure)
+double arrival_time;  // Time for next event #1 (arrival)
+double departure_time; //Time for next event #2 (departure)
 unsigned int n; //Number of customers in the system
 unsigned int c;  // Number of service completions
-double b; // Total busy time
-double s;//  number of customers in system
-double tn = time; // Variable for "last event time"
+double total_busy_time; // Total busy time
+double customers_in_system;//  number of customers in system
+double last_event_time = time; // Variable for "last event time"
 double tb; // Variable for "last start of busy time"
 double x; // Throughput
 double u; // Utilization
@@ -40,60 +40,61 @@ double exponential(double x);
 };
 
 void MM1::initialize()
-{    end_time = SIM_TIME; // Total time to simulate
-     Ta = ARR_TIME; // Mean time between arrivals
-     Ts = SERV_TIME; // Mean service time
-	 n = 0; // Number of customers in the system
-     c = 0; // Number of service completions 
- 	 b = 0.0; // Total busy time
-     s = 0.0; // Area of number of customers in system
-     time = 0.0; // Simulation time
-     t1 = 0.0; // Time for next event #1 (arrival)
-     t2 = SIM_TIME; // Time for next event #2 (departure)
-	 tn = time; 
+{    	end_time = SIM_TIME; // Total time to simulate
+     	Ta = ARR_TIME; // Mean time between arrivals
+     	Ts = SERV_TIME; // Mean service time
+	n = 0; // Number of customers in the system
+     	c = 0; // Number of service completions 
+ 	b = 0.0; // Total busy time
+     	customers_in_system = 0.0; // Number of customers in the system
+     	time = 0.0; // Simulation time
+     	arrival_time = 0.0; // Time for next event #1 (arrival)
+     	departure_time = SIM_TIME; // Time for next event #2 (departure)
+	last_event_time = time; 
 }
 
 void MM1::simulate()
-{ while (time < end_time)
-{
-if (t1 < t2) // *** Event #1 (arrival) ***
-{
-time = t1;
-s = s + n * (time - tn); // Update area under "s" curve
-n++;
-tn = time; // tn = "last event time" for next event
-t1 = time + exponential(Ta);
-if (n == 1)
-{
-tb = time; // Set "last start of busy time"
-t2 = time + exponential(Ts);
-}
-}
-else // *** Event #2 (departure) ***
-{
-time = t2;
-s = s + n * (time - tn); // Update area under "s" curve
-n--;
-tn = time; // tn = "last event time" for next event
-c++; // Increment number of completions
-if (n > 0)
-t2 = time + exponential(Ts);
-else
-{
-t2 = SIM_TIME;
-b = b + time - tb; // Update busy time sum if empty
-}
-}
-}
+{ 
+	while (time < end_time)
+	{
+		if (arrival_time < departure_time) // *** Event #1 (arrival) ***
+		{
+			time = arrival_time;
+			customers_in_system = customers_in_system + n * (time - tn); // Update area under "s" curve
+			n++;
+			last_event_time = time; // last_event_time = "last event time" for next event
+			arrival_time = time + exponential(Ta);
+			if (n == 1)
+			{
+				tb = time; // Set "last start of busy time"
+				departure_time = time + exponential(Ts);
+			}
+		}
+		else // *** Event #2 (departure) ***
+		{
+			time = departure_time;
+			customers_in_system = customers_in_system + n * (time - tn); // Update area under "s" curve
+			n--;
+			last_event_time = time; // tn = "last event time" for next event
+			c++; // Increment number of completions
+			if (n > 0)
+			departure_time = time + exponential(Ts);
+			else
+			{
+				departure_time = SIM_TIME;
+				total_busy_time = total_busy_time + time - tb; // Update busy time sum if empty
+			}
+		}
+	}
 
 // End of simulation so update busy time sum
-b = b + time - tb;
+	total_busy_time = total_busy_time + time - tb;
 
 // Compute outputs
-x = c / time; // Compute throughput rate
-u = b / time; // Compute server utilization
-l = s / time; // Compute mean number in system
-w = l / x; // Compute mean residence or system time
+	x = c / time; // Compute throughput rate
+	u = total_busy_time / time; // Compute server utilization
+	l = customers_in_system / time; // Compute mean number in system
+	w = l / x; // Compute mean residence or system time
 	
 }
 
@@ -104,32 +105,32 @@ double MM1::exponential(double x){
 // Pull a uniform RV (0 < z < 1)
 do
 {
-z =z = ((double) rand() / RAND_MAX);
+	z =z = ((double) rand() / RAND_MAX);
 }
-while ((z == 0) || (z == 1));
+	while ((z == 0) || (z == 1));
 
-return(-x * log(z));
+	return(-x * log(z));
 }
 
 void MM1::report()
 {
-cout<<"========================================== ===================== \n"<<endl;
-cout<<"= *** Results from M/M/1 simulation *** = \n"<<endl;
-cout<<"========================================== ===================== \n"<<endl;
-cout<<"= Total simulated time =  "<< end_time<<"sec "<<endl;
-cout<<"========================================== ===================== \n"<<endl;
-cout<<"= INPUTS: \n";
-cout<<"= Mean time between arrivals = "<< Ta<<"  sec "<<endl;
-cout<<"= Mean service time = "<<Ts<<"  sec "<<endl;
-cout<<"========================================== ===================== \n"<<endl;
-cout<<"= OUTPUTS: \n"<<endl;
-cout<<"= Number of completions = "<<c<<"  cust "<<endl;
-cout<<"= Throughput rate = "<<x<<"  cust/sec "<<endl;
-cout<<"= Server utilization = "<< u<<endl;
-cout<<"= Mean  number of customer in system = "<< l<<"  cust "<<endl;
-cout<<"= Mean residence time = " << w<<"  sec "<<endl;
+	cout<<"========================================== ===================== \n"<<endl;
+	cout<<"= *** Results from M/M/1 simulation *** = \n"<<endl;
+	cout<<"========================================== ===================== \n"<<endl;
+	cout<<"= Total simulated time =  "<< end_time<<"sec "<<endl;
+	cout<<"========================================== ===================== \n"<<endl;
+	cout<<"= INPUTS: \n";
+	cout<<"= Mean time between arrivals = "<< Ta<<"  sec "<<endl;
+	cout<<"= Mean service time = "<<Ts<<"  sec "<<endl;
+	cout<<"========================================== ===================== \n"<<endl;
+	cout<<"= OUTPUTS: \n"<<endl;
+	cout<<"= Number of completions = "<<c<<"  cust "<<endl;
+	cout<<"= Throughput rate = "<<x<<"  cust/sec "<<endl;
+	cout<<"= Server utilization = "<< u<<endl;
+	cout<<"= Mean  number of customer in system = "<< l<<"  cust "<<endl;
+	cout<<"= Mean residence time = " << w<<"  sec "<<endl;
 
-cout<<"========================================== ===================== \n";
+	cout<<"========================================== ===================== \n";
 	
 }
 	
