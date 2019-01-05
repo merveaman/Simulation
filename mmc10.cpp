4f511d08b55df01648db091e266acfbbabc8c35c
merveaman <steamed.zeppelin@gmail.com>
2019-01-05 16:24:37 +0200

    Update mmc10.cpp

--- mmc10.cpp
+++ mmc10.cpp
@@ -32,15 +32,15 @@ int m;
 
 double delay; // waiting time in queue
 double time; // Simulation time
-double t1 ; // Time for next event #1 (arrival)
-double t2 ; // Time for next event #2 (departure)
-double t3; // Time for next event #3 (departure)
+double arrival_time; // Time for next event #1 (arrival)
+double departure_time; // Time for next event #2 (departure)
+double departure_time2; // Time for next event #3 (departure)
 long int n; // Number of customers in the system in the queue
 long int queue_size; //buffer space or queue size
 double c ; // Number of service completions
-double s ; //  number of customers in system
-double b ; // Total busy time
-double tn ; // Variable for "last event time"
+double customers_in_system; //  number of customers in system
+double total_busy_time; // Total busy time
+double last_event_time ; // Variable for "last event time"
 double tb; // Variable for "last start of busy time"
 double x; // Throughput
 double l; // Mean number in the system
@@ -69,15 +69,15 @@ server_status1=0;
 server_status2=0;
 m=2;
  time = 0.0; // Simulation time
- t1 = 0.0; // Time for next event #1 (arrival)
- t2 = SIM_TIME; // Time for next event #2 (departure1)
- t3= SIM_TIME;  //  Time for next event #3 (departure2)
+ arrival_time = 0.0; // Time for next event #1 (arrival)
+ departure_time = SIM_TIME; // Time for next event #2 (departure1)
+ departure_time2= SIM_TIME;  //  Time for next event #3 (departure2)
  n = 0; // Number of customers in the system
  queue_size=10; //buffer space or queue size
  c = 0.0; // Number of service completions
- s = 0.0; // Area of number of customers in system
- b = 0.0; // Total busy time
- tn = time; // Variable for "last event time"
+ customers_in_system = 0.0; // Area of number of customers in system
+ total_busy_time = 0.0; // Total busy time
+ last_event_time = time; // Variable for "last event time"
 
 }
 
@@ -85,9 +85,9 @@ m=2;
 void MMC::simulate(){
 	while (time < end_time)
 { 
-if (t1 < t2)	{  // arrival process
-time = t1;
-s = s + n * (time - tn); // Update area under "s" curve
+if (arrival_time < departure_time)	{  // arrival process
+time = arrival_time;
+customers_in_system = customers_in_system + n * (time - last_event_time); // Update area under "s" curve
 if (server_status1==BUSY && server_status2==BUSY){
              n++;
              
@@ -101,10 +101,10 @@ if (n > queue_size)
         }
 
 
-tn = time; // tn = "last event time" for next event
-t1 = time + expntl(Ta);	
+last_event_time = time; // tn = "last event time" for next event
+arrival_time = time + expntl(Ta);	
 tb = time;
-t2 = time + expntl(Ts);
+departure_time = time + expntl(Ts);
 
 
 }
@@ -114,9 +114,9 @@ t2 = time + expntl(Ts);
     	server_status2= BUSY;
          
         tn = time; // tn = "last event time" for next event
-t1 = time + expntl(Ta);	
+arrival_time = time + expntl(Ta);	
 tb = time;
-t2 = time + expntl(Ts);
+departure_time = time + expntl(Ts);
 
 	}
 	else if (server_status1 == IDLE &&  server_status2==BUSY)
@@ -124,10 +124,10 @@ t2 = time + expntl(Ts);
     	server_status1= BUSY;
          
           
-tn = time; // tn = "last event time" for next event
-t1 = time + expntl(Ta);	
+last_event_time = time; // tn = "last event time" for next event
+arrival_time = time + expntl(Ta);	
 tb = time;
-t3 = time + expntl(Ts);
+departure_time2 = time + expntl(Ts);
 
 	}
  
@@ -137,11 +137,11 @@ t3 = time + expntl(Ts);
         server_status2= BUSY;
          
          
-tn = time; // tn = "last event time" for next event
-t1 = time + expntl(Ta);	
+last_event_time = time; // tn = "last event time" for next event
+arrival_time = time + expntl(Ta);	
 tb = time;
-t2 = time + expntl(Ts);
-t3=  time + expntl(Ts);
+departure_time = time + expntl(Ts);
+departure_time2=  time + expntl(Ts);
 
          
     }
@@ -151,26 +151,26 @@ t3=  time + expntl(Ts);
 
 else{ // departure process from server 1 and server 2
 	
-time = t2;
-s = s + n * (time - tn); 
+time = departure_time;
+customers_in_system = customers_in_system + n * (time - last_event_time); 
 if (n==0)
 {          server_status1= IDLE;
            server_status2= IDLE;
-           t2 = end_time;
-           t3 = end_time;
-           b=b+time-tb;
+           departure_time = end_time;
+           departure_time2 = end_time;
+           total_busy_time=total_busy_time+time-tb;
 	
 }
 
 else{
 	--n;  
-	tn = time; // tn = "last event time" for next event
+	last_event_time = time; // tn = "last event time" for next event
     c++;    
-	t2 = time + expntl(Ts);
-	t3 = time + expntl(Ts);
-	b=b+time-tb;
+	departure_time = time + expntl(Ts);
+	departure_time2 = time + expntl(Ts);
+	total_busy_time=total_busy_time+time-tb;
 	lq++;
-    delay= t1-time;
+    delay= arrival_time-time;
     wq+= delay;
 }
  
@@ -181,9 +181,9 @@ else{
 }
 // End of simulation so update busy time sum
 
-b = b + time - tb;
+total_busy_time = total_busy_time + time - tb;
 x = c / time; // Compute throughput rate
-l = s / time; // Compute mean number in system
+l = customers_in_system / time; // Compute mean number in system
 w = l / x; // Compute mean residence or system time
 u = b/(time*m); // Compute server utilization
 lq=lq/time;     //compute mean queue length
